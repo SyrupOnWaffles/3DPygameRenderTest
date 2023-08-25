74e01a854c740dca93668fa7ea41be05f1053bb7
syruponwaffles <nicholaskor@hotmail.com>
2023-08-25 14:18:45 -0600

    backface culling

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ xAngle = np.deg2rad(180)
 yAngle = np.deg2rad(0)
 zAngle = np.deg2rad(0)
 screenSize = [256, 256]
+
 class colours:
     black = (40,40,46)
     purple = (108,86,113)
@@ -14,6 +15,8 @@ class colours:
     red = (249,130,132)
     light_purple = (176,169,228)
     blue = (172,204,228)
+    mediumBlue = (137,163,182)
+    darkBlue = (68,81,91)
     teal = (179,227,218)
     pink = (254,170,228)
     dark_green = (135,168,137)
@@ -39,16 +42,13 @@ zRotation = [[np.cos(zAngle), -np.sin(zAngle), 0],
                 [0, 0, 1]]    
 
 
-background_colour = colours.black
-
 screen = pygame.display.set_mode((screenSize[0], screenSize[1]), SCALED, 1)
 
 texture = pygame.image.load("texture.png")
 # Set the caption of the screen
 pygame.display.set_caption('3D thing!')
   
-screen.fill(background_colour)
-def toProjected2D(coords, xAngle, yAngle, zAngle):
+def rotated(coords, xAngle, yAngle, zAngle):
     xRotation = [[1, 0, 0],
                 [0, np.cos(xAngle), -np.sin(xAngle)],
                 [0, np.sin(xAngle), np.cos(xAngle)]]
@@ -64,33 +64,82 @@ def toProjected2D(coords, xAngle, yAngle, zAngle):
     rotated = np.matmul(coords, xRotation)
     rotated = np.matmul(rotated, yRotation)
     rotated = np.matmul(rotated, zRotation)
-
-    projected2D = np.matmul(rotated, orthProjection)
             
+    return rotated
+
+def toProjected2D(coords):
+    projected2D = np.matmul(coords, orthProjection)
     return projected2D
 
-def renderObject(model, xAngle, yAngle, zAngle, colour, thickness):      
-    for x in range(len(model.faces)):        
-            one = toProjected2D(model.vertices[(model.faces[x][0][0]) - 1], xAngle, yAngle, zAngle)
-            two = toProjected2D(model.vertices[(model.faces[x][1][0]) - 1], xAngle, yAngle, zAngle)
-            three = toProjected2D(model.vertices[(model.faces[x][2][0]) - 1], xAngle, yAngle, zAngle)
-            pygame.gfxdraw.aapolygon(screen, [((one[0][0]) + (screenSize[0] / 2), (one[0][1]) + (screenSize[1] / 2)), ((two[0][0]) + (screenSize[0] / 2), (two[0][1]) + (screenSize[1] / 2)), ((three[0][0]) + (screenSize[0] / 2), (three[0][1]) + (screenSize[1] / 2))], colour)
-    for y in range(len(model.normal)):
-            pixel = toProjected2D(model.normal[y], xAngle, yAngle, zAngle)
-            pygame.gfxdraw.pixel(screen, int(pixel[0][0]+ (screenSize[0] / 2)) , int(pixel[0][1] + (screenSize[1] / 2)), colours.green)
+# first color is light, then medium then dark
+def renderObject(model, xAngle, yAngle, zAngle, colours, thickness):      
+    maxNormal = 0
+    minNormal = 1000
+    for x in range(len(model.faces)): 
+            shade = 0
+            one = [0,0,0]
+            one = rotated(model.vertices[(model.faces[x][0][0]) - 1], xAngle, yAngle, zAngle)
+            two = rotated(model.vertices[(model.faces[x][1][0]) - 1], xAngle, yAngle, zAngle)
+            three = rotated(model.vertices[(model.faces[x][2][0]) - 1], xAngle, yAngle, zAngle)
+            # normal calc
+            normal = [0,0,0]
+            line1 = [0,0,0]
+            line2 = [0,0,0]
+            line1[0] = two[0][0] - one[0][0]
+            line1[1] = two[0][1] - one[0][1]
+            line1[2] = two[0][2] - one[0][2]
+        
+            line2[0] = three[0][0] - one[0][0]
+            line2[1] = three[0][1] - one[0][1]
+            line2[2] = three[0][2] - one[0][2]
+        
+            normal[0] = line1[1] * line2[2] - line1[2] * line2[1]
+            normal[1] = line1[2] * line2[0] - line1[0] * line2[2]
+            normal[2] = line1[0] * line2[1] - line1[1] * line2[0]
+        
+            l = np.sqrt(normal[0]*normal[0] + normal[1]*normal[1] + normal[2]*normal[2]);
+            normal[0] /= l
+            normal[1] /= l
+            normal[2] /= l
+                
+            if(normal[2] > 0):
+                if(normal[2] > maxNormal):
+                    maxNormal = normal[2]
+                elif(normal[2] < minNormal):
+                    minNormal = normal[2]
+                one = toProjected2D(one)
+                two = toProjected2D(two)
+                three = toProjected2D(three)
+                
+
+                # shader color
+                if( normal[0]>= .7):
+                    shade=0
+                elif( normal[0]>= -.5):
+                    shade=1
+                else:
+                    shade=2
+                pygame.gfxdraw.filled_polygon(screen, [((one[0][0]) + (screenSize[0] / 2), (one[0][1]) + (screenSize[1] / 2)), ((two[0][0]) + (screenSize[0] / 2), (two[0][1]) + (screenSize[1] / 2)), ((three[0][0]) + (screenSize[0] / 2), (three[0][1]) + (screenSize[1] / 2))], colours[shade])
+
 pygame.display.flip()
 
 running = True
 # game loop
 while running:
-    screen.fill(background_colour)
-    yAngle += .05
-    xAngle += .05
-
+    screen.fill(colours.black)
     for event in pygame.event.get():
         # Check for QUIT event      
         if event.type == pygame.QUIT:
             running = False
-    renderObject(model5, xAngle, yAngle, zAngle, colours.grey, 0)
+    keys = pygame.key.get_pressed()  # Checking pressed keys
+    if keys[pygame.K_LEFT]:
+        yAngle -= .01
+    if keys[pygame.K_RIGHT]:
+        yAngle += .01
+    if keys[pygame.K_UP]:
+        xAngle -= .01
+    if keys[pygame.K_DOWN]:
+        xAngle += .01
+    renderObject(model5, xAngle, yAngle, zAngle, [colours.white,colours.tan,colours.orange], 0)
     
     pygame.display.update()
--- model4.py
+++ model4.py
@@ -1,6 +1,6 @@
 #pyramid
 class transform:
-    size = 10
+    size = 20
     normalsize = 20
     xPos = 0
     yPos = 0
@@ -17,10 +17,4 @@ faces=[
     [[1,4,2],[2,5,2],[3,6,2]],
     [[2,3,3],[4,2,3],[3,7,3]],
     [[3,7,4],[4,2,4],[1,1,4]],
-]
-normal=[
-    [[int(0.8402 * transform.normalsize) + transform.xPos, int(0.2425 * transform.normalsize) + transform.yPos, int(-0.4851 * transform.normalsize) + transform.zPos]],
-    [[int(0.0000 * transform.normalsize) + transform.xPos, int(-1.0000 * transform.normalsize) + transform.yPos, int(0.0000 * transform.normalsize) + transform.zPos]],
-    [[int(-0.0000 * transform.normalsize) + transform.xPos, int(0.2425 * transform.normalsize) + transform.yPos, int(0.9701 * transform.normalsize) + transform.zPos]],
-    [[int(-0.8402 * transform.normalsize) + transform.xPos, int(0.2425 * transform.normalsize) + transform.yPos, int(-0.4851 * transform.normalsize) + transform.zPos]],
 ]
